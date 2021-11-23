86e30977828f3a92474f7a22c0b9da75b38d3d06
Kames Geraghty <kamescg@gmail.com>
2021-11-23 12:58:30 +0000

    update(handler): use new config interface

--- src/handler.ts
+++ src/handler.ts
@@ -6,40 +6,28 @@ const debug = require('debug')('pt-autotask')
 export async function handler(event: any) {
   const { infuraApiKey } = event.secrets;
   const relayer = new Relayer(event);
-
   const config = {
-    network: "rinkeby",
-    chainId: 4,
-    apiKey: infuraApiKey,
     speed: "fast",
     gasLimit: 50000,
     execute: false,
     L1: {
       chainId: 4,
-      network: 'rinkeby',
+      providerUrl: `https://rinkeby.infura.io/v3/${infuraApiKey}`,
     },
     L2: {
-      chainId: 80001,
-      network: 'polygon-mumbai',
-      apiKey: infuraApiKey,
+      chainId: 43113,
       providerUrl: 'https://api.avax-test.network/ext/bc/C/rpc',
-      speed: "fast",
-      gasLimit: 50000,
-      execute: false,
     },
   }
 
   const { msg: L2Msg, err: L2Error, status: L2Status, transaction: L2Transaction } = await L2DrawAndPrizeDistributionPush(contracts, config, relayer)
   if (L2Error && L2Status == -1) console.log(L2Error);
-  console.log("L2PrizeDistributionPush:", L2Msg)
   debug("L2Msg:", L2Msg)
   debug("L2Error:", L2Error)
   debug("L2Status:", L2Status)
   if (L2Error && L2Status == -1) console.log(L2Error);
   if (L2Status == 1) {
     console.log('Executing:', L2Msg)
-    console.log(relayer)
-
     let L2tx = await relayer.sendTransaction({
       data: L2Transaction.data,
       to: L2Transaction.to,
